dddf2d3a80a3424a707ffcfaed8b79e131a59e99
TPHRAB <chenze.zhao0@outlook.com>
2019-07-28 23:16:47 +0800

    add AutoDownload class which can automatically download the video from the url

--- DownloadManager/src/extensions/download/AutoDownload.java
+++ DownloadManager/src/extensions/download/AutoDownload.java
@@ -1,4 +1,59 @@
 package extensions.download;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.File;
+import java.net.URL;
+import java.sql.SQLOutput;
+import java.util.*;
+
 public class AutoDownload {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("javax.net.ssl.keyStore", "icekeystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
+
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please input the video url: ");
+        Document doc = Jsoup.connect(console.nextLine()).get();
+        String link = doc.getElementsByTag("source").get(0).attributes().get("src");
+
+        File out1 = new File("C:\\Users\\Timmy\\Desktop\\tmp.m3u8");
+        out1.createNewFile();
+        out1.deleteOnExit();
+        doDownloadSingleFile(new URL(link), out1, 1);
+
+        Scanner read = new Scanner(out1);
+        String file = null;
+        while (read.hasNextLine()) {
+            file = read.nextLine();
+        }
+        link = link.substring(0, link.lastIndexOf('/') + 1) + file;
+
+        File out2 = new File("C:\\Users\\Timmy\\Desktop\\result.m3u8");
+        out2.createNewFile();
+        doDownloadSingleFile(new URL(link), out2, 1);
+        doDownloadFileList(link, out2, "C:\\Users\\Timmy\\Desktop\\raw", 1);
+
+    }
+    public static void doDownloadSingleFile(URL url, File out, int threadNum) throws Exception {
+        Download threads = new Download(url, out, threadNum);
+        threads.start();
+        threads.join();
+    }
+
+    public static void doDownloadFileList(String link, File m3u8, String dir, int threadNum) throws Exception {
+        Scanner read = new Scanner(m3u8);
+        while (read.hasNextLine()) {
+            String line = read.nextLine();
+            if (line.charAt(0) != '#') {
+                File out = new File(dir + "\\" + line);
+                URL url = new URL(link.substring(0, link.lastIndexOf('/') + 1) + line);
+                doDownloadSingleFile(url, out, 1);
+            }
+        }
+    }
 }
--- DownloadManager/src/extensions/download/FindTargetLink.java
+++ DownloadManager/src/extensions/download/FindTargetLink.java
@@ -1,4 +1,20 @@
 package extensions.download;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
 public class FindTargetLink {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("javax.net.ssl.keyStore", "icekeystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
+
+        Document doc = Jsoup.connect("https://avgle.com/video/50J8w82c585/200gana-1791-%E3%83%AF%E3%83%BC%E3%83%AB%E3%83%89%E3%82%AB%E3%83%83%E3%83%97%E8%A6%B3%E6%88%A6%E3%83%8A%E3%83%B3%E3%83%91-%E3%82%B5%E3%83%83%E3%82%AB%E3%83%BC%E6%97%A5%E6%9C%AC%E4%BB%A3%E8%A1%A8-%E5%88%9D%E6%88%A6%E5%8B%9D%E5%88%A9%E3%81%AE%E7%86%B1%E7%8B%82%E3%81%AB%E3%81%8B%E3%81%93%E3%81%A4%E3%81%91%E3%81%A6-%E8%A6%B3%E6%88%A6%E3%81%AB%E8%A8%AA%E3%82%8C%E3%81%9F%E3%83%A2%E3%83%87%E3%83%AB%E7%B4%9A%E7%BE%8E%E5%A5%B3%E3%82%B5%E3%83%9D%E3%83%BC%E3%82%BF%E3%83%BC2%E4%BA%BA%E3%81%AB%E5%A3%B0%E3%82%92%E3%81%8B%E3%81%91-%E3%83%9B%E3%83%86%E3%83%AB%E3%81%A7%E7%BE%8E%E9%85%92%E3%81%AB%E9%85%94%E3%81%84-%E8%88%88%E5%A5%AE%E5%86%B7%E3%82%81%E3%82%84%E3%82%89%E3%81%AC%E3%81%BE%E3%81%BE-%E6%BF%80%E3%82%A4%E3%82%AD%E7%B5%B6%E9%A0%824p%E4%B9%B1%E4%BA%A4%E3%82%BB%E3%83%83%E3%82%AF%E3%82%B9-%E3%81%AF%E3%82%8B%E3%82%89-22%E6%AD%B3-%E3%82%A2%E3%83%91%E3%83%AC%E3%83%AB%E5%A4%A7%E6%89%8B%E4%BC%81%E6%A5%AD%E3%81%AE%E3%83%91%E3%82%BF%E3%83%B3%E3%83%8A%E3%83%BC-%E3%81%BF%E3%82%8C%E3%81%84-22%E6%AD%B3-%E3%82%A2%E3%83%91%E3%83%AC%E3%83%AB%E5%A4%A7%E6%89%8B%E4%BC%81%E6%A5%AD%E3%81%AE%E5%95%86%E5%93%81%E7%AE%A1%E7%90%86").get();
+        Elements list = doc.getAllElements();
+        for (Element e : list) {
+            System.out.println(e);
+        }
+    }
 }
