e06e36f997bd838fffba493f0596d6e1871b7a69
Tim <2115124830@qq.com>
2019-08-05 17:36:40 +0800

    fix the bug of cannot convert .ts to .mp4 automatically

--- DownloadManager/src/extensions/download/AutoDownload.java
+++ DownloadManager/src/extensions/download/AutoDownload.java
@@ -7,6 +7,8 @@ import org.jsoup.nodes.Document;
 import java.util.regex.*;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.net.URL;
@@ -24,6 +26,8 @@ public class AutoDownload {
         // set up ssl
         System.setProperty("javax.net.ssl.keyStore", "icekeystore.jks");
         System.setProperty("javax.net.ssl.keyStorePassword", "123456");
+        System.setProperty("javax.net.ssl.trustStore", "icetruststore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
 
         // get url and output path
         Scanner console = new Scanner(System.in);
@@ -38,45 +42,47 @@ public class AutoDownload {
 
         // 要得到父节点判断是哪个方法
         if(host.attributeValue("method").equals("m3u8Append")) {
-//            m3u8Appended(url, out, host);
+            m3u8Appended(url, out, host);
             System.out.print("Please input the path for combination: ");
             String path = console.nextLine();
             path = path + DownloadManager.getURLTitle(url) + ".ts";
-//            extensions.copy.TestThread.waitThreads(
-//                    extensions.copy.TestThread.doCombine(out.listFiles(),
-//                            new File(path)));
-            // 初始化参数
-            String p1 = "\"" + path + "\"" ;
-            String p2 = "\"" + path.substring(0, path.length() - 3) + ".mp4" + "\"";
+            File result = new File(path);
+            extensions.copy.TestThread.waitThreads(
+                    extensions.copy.TestThread.doCombine(out.listFiles(), result));
+            
+            if (out.getName().equals("raw")) {
+            	for (File f : out.listFiles()) {
+            		f.delete();
+            	}
+            }
+            
             // 得到系统
             String os = System.getProperty("os.name").toLowerCase();
-            String system = null;
-            String command = null;
-            boolean execution = true;
-            if (os.contains("mac")) {
-            	system = "macos";
-            	command = "./ffmpeg -i ";
-            } else if (os.contains("win")) {
-            	system="windows";
-            	command = "cmd.exe /c start ";
+            List<String> command = new ArrayList<>();
+            boolean execution = false;
+            if (os.contains("win")) {
+            	command.add("cmd.exe");
+            	command.add("/c"); 
+            	command.add("start");
+            	command.add("./ffmpeg.exe");
+            	execution = true;
+            } else if (os.contains("mac")) {
+            	command.add("./ffmpeg");
+            	execution = true;
             } else {
-            	execution = false;
             	System.out.println("Not supported operating system! Please do the convertion by yourself!");
             }
+            
+            // set input file and output file
+            command.add("-i");
+            command.add(path);
+            command.add(path.substring(0, path.length() - 2) + "mp4");
+            
             // run script
             if (execution) {
-	            String script = xml.selectSingleNode("/config/convert/" + system).getText();
-	            // ProcessBuilder pb = new ProcessBuilder("./" + script, p1, p2);
-	            command = command + script + " " + p1 + " " + p2;
-	            Process process = Runtime.getRuntime().exec(command);
-	    		BufferedReader r = new BufferedReader(new InputStreamReader(process.getInputStream()));
-	    		String nextLine = r.readLine();
-	    		while(nextLine != null) {
-	    			System.out.println(nextLine);
-	    			nextLine = r.readLine();
-	    		}
-	    		process.waitFor();
+            	videoConvert(command);
             }
+            result.delete();
         } else {
             System.out.print("Please input the first common part of the sequence url: ");
             String part1 = console.nextLine().trim();
@@ -116,6 +122,9 @@ public class AutoDownload {
                 .get(Integer.parseInt(host.element("index").getTextTrim()))
                 .attributes()
                 .get(host.element("attribute").getTextTrim());
+        if (host.attributeValue("addProtocal").equals("true")) {
+        	link = l.substring(0, l.indexOf('/')) + link;
+        }
         link = link.substring(link.lastIndexOf("http"));
         URL url = new URL(link);
         File out = new File("./" + DownloadManager.getURLFileName(url));
@@ -134,14 +143,12 @@ public class AutoDownload {
             }
             link = link.substring(0, link.lastIndexOf('/') + 1) + file;
             url = new URL(link);
-            File out2 = new File("./" + DownloadManager.getURLFileName(url));
+            out = new File("./" + DownloadManager.getURLFileName(url));
             pb = new ProgressBar("Download", 1, "Threads", 1, "1");
             pb.start();
-            DownloadManager.doDownloadSingleFile(url, out2, 1, pb.getPipedWriter());
+            DownloadManager.doDownloadSingleFile(url, out, 1, pb.getPipedWriter());
             pb.join();
             // set "out" to be "out2"
-            out.delete();
-            out = out2;
             read.close();
         }
 
@@ -183,5 +190,26 @@ public class AutoDownload {
         read.close();
         return list;
     }
-
+    
+	public static void videoConvert(List<String> command) throws IOException {
+		// 执行操作
+		ProcessBuilder builder = new ProcessBuilder(command);
+		Process process = builder.start();
+		InputStream errorStream = process.getErrorStream();
+		InputStreamReader isr = new InputStreamReader(errorStream);
+		BufferedReader br = new BufferedReader(isr);
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+		if (br != null) {
+			br.close();
+		}
+		if (isr != null) {
+			isr.close();
+		}
+		if (errorStream != null) {
+			errorStream.close();
+		}
+	}
 }
