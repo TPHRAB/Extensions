dcf6b675fce09d725bc0b818c83e9cc6e10f3241
Tim <2115124830@qq.com>
2019-08-16 14:08:52 +0800

    enable adding additional information like file size at the end of the progress bar

--- DownloadManager/src/extensions/download/ProgressBarThread.java
+++ DownloadManager/src/extensions/download/ProgressBarThread.java
@@ -7,25 +7,22 @@ public class ProgressBarThread implements Runnable {
     private PipedReader pR;
     private long total;
     private String taskName;
-    private String unit;
-    private int num;
-    private String threadNum;
+    private int unit;
+    private String additionalInformation;
     private Thread thread;
 
-    public ProgressBarThread(PipedReader pR, String taskName, long total, String unit, int num, String threadNum) {
-        this.threadNum = threadNum;
+    public ProgressBarThread(PipedReader pR, String taskName, long total, String additionalInfo, int unit, String info) {
         this.pR = pR;
         this.taskName = taskName;
         this.total = total;
         this.unit = unit;
-        this.num = num;
+        this.additionalInformation = " | " + additionalInfo + ": " + info;
         this.thread = new Thread(this);
     }
 
     @Override
     public void run() {
-        System.out.print("[*** " + taskName + ": 0% " + "(0/" + total + "), " + total + " " + unit + " | Threads: "
-                + threadNum + " ***]");
+        System.out.print("[*** " + taskName + ": 0% " + "(0/" + total + "), " + total + " threads" + additionalInformation + " ***]");
         long current = 0;
         long counter = 0;
         long start = System.currentTimeMillis();
@@ -36,15 +33,15 @@ public class ProgressBarThread implements Runnable {
                 for (int i = 0; i < count; i++) {
                     counter += buffer[i];
                 }
-                current = counter / num;
+                current = counter / unit;
                 double currentRounded = Math.round(current * 100.0 / total);
                 System.out.print("\r" + "[*** " + taskName + ": " + currentRounded + "% ("
-                        + current + "/" + total + "), " + "Total: " + total + " " + unit  + " | Threads: "
-                        + threadNum + " ***]");
+                        + current + "/" + total + "), " + "Total: " + total + " threads"
+                		+ additionalInformation + " ***]");
             } catch (Exception e) {}
         }
         System.out.println("\r" + taskName + ": " + 100 + "% (" + current + "/" + total +
-                "), " + total + " " + unit  + " | Threads: " + threadNum + ", done.");
+                "), " + total + " threads"  + additionalInformation + ", done.");
         System.out.println(getBold("Time consumed: " + (System.currentTimeMillis() - start) / 1000.0
                 +  " seconds"));
     }
